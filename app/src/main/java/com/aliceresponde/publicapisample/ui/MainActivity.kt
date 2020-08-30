f717e3bd9411ec78128029e4f963ac1c3f1f209d
Alicia Beltran <aliceresponde@gmail.com>
2020-08-29 21:27:41 -0500

    gradle setup and  some xmls

---
+++ app/src/main/java/com/aliceresponde/publicapisample/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.aliceresponde.publicapisample.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.aliceresponde.publicapisample.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
